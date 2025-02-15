8482562721a973a2c8a0d7633d3ec60f97c2307c
Andrea Froncillo <andreafroncillo0@gmail.com>
2025-02-15 14:38:52 +0100

    Add files via upload

---
+++ selfwork_cicli_2/main.js
@@ -0,0 +1,19 @@
+let num = 0;
+
+let somma = 1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19;
+
+let n = 10
+
+let media = somma/n
+
+for(i=1; i<=20; i++){
+    if(i%2 == 0){
+        console.log(i)  
+    }
+
+};
+
+console.log(media);
+
+
+
